bdf7bd8b8fdea5a7bb99c53ce597d698f9627cd5
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-01-22 13:19:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/' , (req ,res)=>{
+      res.send("server is running");
+})
+
+app.listen(port , ()=>{
+      console.log(`your server is running on http://localhost:3000`)
+});
