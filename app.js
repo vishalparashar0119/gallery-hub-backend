9f105e2137c33e02221f8761c2b5330295502922
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-01-22 17:28:21 +0530

    add common middle ware for better data readablity

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ env.config();
 
 import express from 'express';
 import connectDb  from './database/dataBase.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 3000;
 await connectDb();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 
 app.get('/' , (req ,res)=>{
       res.send("server is running");
