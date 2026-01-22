3232485e643fc05d70c625730876908349647761
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-01-22 17:32:38 +0530

    created admin model

---
+++ models/adminModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+
+const adminSchema = mongoose.Schema({
+      email : {type : String , require : true},
+      password : { type : String , require : true}
+})
+
+const AdminModel = mongoose.model('admin' , adminSchema)
